ee66f478175da697f784dd914ab4580536aa5f8b
Ramsha42001 <90575580+Ramsha42001@users.noreply.github.com>
2025-01-02 17:31:51 +0530

    updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Disable unused variables rule
+      "react/no-unescaped-entities": "off",       // Disable unescaped entities warning
+      "@next/next/no-img-element": "off",         // Allow usage of <img> instead of <Image>
+    },
+  },
 ];
 
 export default eslintConfig;
